0767281efbd335bad5316caa5e88c1db712beac0
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-21 16:40:43 -0500

    add Employee service

--- src/main/java/edu/cibertec/eva01/service/EmployeeService.java
+++ src/main/java/edu/cibertec/eva01/service/EmployeeService.java
@@ -1,4 +1,35 @@
 package edu.cibertec.eva01.service;
 
+import edu.cibertec.eva01.model.Employee;
+import edu.cibertec.eva01.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.getEmployees();
+    }
+
+    public void saveEmployee(Employee employee) {
+        employeeRepository.saveEmployee(employee);
+    }
+
+    public void updateEmployee(Employee employee) {
+        employeeRepository.updateEmployee(employee);
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.getEmployeeById(id);
+    }
+
+    public void deleteEmployee(Employee employee) {
+        employeeRepository.removeEmployee(employee);
+    }
 }
